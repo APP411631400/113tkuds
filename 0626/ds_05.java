30d3224cfb3094e62d9ab2e77e4f4b8fbbf278bc
APP411631400 <abel12280906@gmail.com>
2025-06-26 12:30:15 +0800

    add 0626
    add 0626

--- 0626/ds_05.java
+++ 0626/ds_05.java
@@ -1,4 +1,12 @@
 
 public class ds_05 {
-    
+    public static void main(String[] args) {
+        int[] numbers = {3, 5, 7, 2, 9, 1, 4, 6, 8, 10};
+        int sum = 0;
+        for (int i = 0; i < numbers.length; i++) {
+            sum += numbers[i];
+        }
+        System.out.println("總和為：" + sum);
+    }
 }
+
--- 0626/ds_06.java
+++ 0626/ds_06.java
@@ -1,5 +1,23 @@
 
 
+import java.util.Random;
+
 public class ds_06 {
-    
+    public static void main(String[] args) {
+        Random rand = new Random();
+        int[] numbers = new int[10];
+        for (int i = 0; i < numbers.length; i++) {
+            numbers[i] = rand.nextInt(100);
+        }
+
+        int max = numbers[0];
+        for (int i = 1; i < numbers.length; i++) {
+            if (numbers[i] > max) {
+                max = numbers[i];
+            }
+        }
+
+        System.out.println("最大值為：" + max);
+    }
 }
+
