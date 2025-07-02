09b255bf81f9f9058e1abc324dd26dc651915e5e
APP411631400 <abel12280906@gmail.com>
2025-07-02 10:36:59 +0800

    add 0702
    add 0702

--- 0702/FibonacciSimple.java
+++ 0702/FibonacciSimple.java
@@ -8,7 +8,7 @@ public class FibonacciSimple {
         System.out.println("費氏數列前"+ n +"項:");
         for (int i = 0; i < n; i++)
         {
-            System.out.println(a +" ");
+            System.out.print(a +" ");
             int temp = a + b;
             a = b;
             b = temp;
